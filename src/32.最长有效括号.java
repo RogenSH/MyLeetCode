9eec214a4addc0a00e85b23bfc7cffb09aace356
ZhaiYang <ZhaiYang@ZhaiYang.local>
2019-11-23 23:00:21 +0800

    动态规划

---
+++ src/32.最长有效括号.java
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode.cn id=32 lang=java
+ *
+ * [32] 最长有效括号
+ *
+ * https://leetcode-cn.com/problems/longest-valid-parentheses/description/
+ *
+ * algorithms
+ * Hard (28.55%)
+ * Likes:    388
+ * Dislikes: 0
+ * Total Accepted:    28K
+ * Total Submissions: 97.7K
+ * Testcase Example:  '"(()"'
+ *
+ * 给定一个只包含 '(' 和 ')' 的字符串，找出最长的包含有效括号的子串的长度。
+ * 
+ * 示例 1:
+ * 
+ * 输入: "(()"
+ * 输出: 2
+ * 解释: 最长有效括号子串为 "()"
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: ")()())"
+ * 输出: 4
+ * 解释: 最长有效括号子串为 "()()"
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public int longestValidParentheses(String s) {
+        
+    }
+}
+// @lc code=end
+
