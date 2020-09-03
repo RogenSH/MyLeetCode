441039d92919afe54435d8a358a97575ca6bbc4f
Budy <budy@BudydeMacBook-Pro.local>
2020-09-03 10:20:24 +0800

    20.9.3

--- src/300.最长上升子序列.java
+++ src/300.最长上升子序列.java
@@ -33,10 +33,21 @@
  */
 
 // @lc code=start
-class Solution {
-    public int lengthOfLIS(int[] nums) {
-        
+public class Solution {
+  public int lengthOfLIS(int[] nums) {
+    int[] dp = new int[nums.length];
+    int len = 0;
+    for (int num : nums) {
+      int i = Arrays.binarySearch(dp, 0, len, num);
+      if (i < 0) {
+        i = -(i + 1);
+      }
+      dp[i] = num;
+      if (i == len) {
+        len++;
+      }
     }
+    return len;
+  }
 }
 // @lc code=end
-
